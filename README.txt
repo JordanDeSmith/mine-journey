0ee1dff2c971431984c46fdc2bf4a8f6e23130ce
JordanSmith <great.jordini@gmail.com>
2022-02-26 13:05:02 -0700

    change to only holding mine locations

---
+++ README.txt
@@ -0,0 +1,17 @@
+A modified take on Minesweeper for Blinks. 
+Created by Jordan Smith
+
+RULES: 
+
+Instead of trying to clear the board of mines as in traditional Minesweeper, instead you are trying to "get" to the end of the map while avoiding the mines. If you are able to reach the end of the map without hitting a mine, you win! If you do hit a mine, you lose and have to try again. 
+
+SETUP: 
+TBD
+
+GAMEPLAY:
+To make a move, simply remove any Blink from the board, and reattach it wherever you like. If the Blink turns *EMPTY COLOR*, then the space is empty. For any yellow faces, there is a mine adjacent. 1 yellow face indicates 1 mine nearby, 2 yellow faces indicate 2 mines nearby, etc. The direction of the face has no bearing on which way the mine is. 
+If the blink turns green (and subsequently all others) this means you've won! After a couple of seconds they will return to setup.
+If the blink turns red (and subsequently all others) this means you've hit a mine and lost. After a couple of seconds they will return to setup. 
+If you reach the edge of the map, the blink will turn off, except for one face which will point in the direction of the goal. You may not reattach a piece to an out of bounds space.
+At any point you may press a Blink to see an indicator of which direction the end is. 
+Long pressing a Blink will reset all connected Blinks. 
--- mine_journey.ino
+++ mine_journey.ino
@@ -3,50 +3,57 @@
  * By Jordan Smith
  * 
 */
+////Constants////
 #define FLASH_LENGTH 2000
-#define TIMER_LENGTH 2000
+#define END_TIMER_LENGTH 2000
+#define SPIN_TIMER_LENGTH 1000
+#define NO_PARENT 6
+#define MASTER 7
+#define NORTH_COLOR GREEN
+#define READY_GREEN makeColorRGB(0, 225, 25) //TODO: Fix this. It's gross.
+#define EMPTY_COLOR dim(WHITE, 255/4)
+#define MINE_INDICATOR_COLOR YELLOW
+#define MAP_HEIGHT 16
+#define MAP_WIDTH 16
+#define MINE_MAX 30
 
 ////data////
-enum Data {SETUP = 17, SENDING, SENDING_TO_OTHER, RECEIVING, READY, CONNECTED, DISCONNECTED, GAME_OVER, VICTORY, WON, LOST, EMPTY, MINE, END, START, PLAYING};
-byte state = SETUP;
-Timer endTimer;
-
-////Colors////
-Color northColor = GREEN;
-Color emptyColor = dim(WHITE, 255/4);
+enum Data {SETUP = 50, SENDING, SENDING_TO_OTHER, RECEIVING, READY, CONNECTED, DISCONNECTED, GAME_OVER, VICTORY, WON, LOST, EMPTY, MINE, END, START, PLAYING};
+static byte state = SETUP;
+static Timer endTimer;
 
 ////Parents and children for map propogation////
-const byte NO_PARENT = 6;
-const byte MASTER = 7;
-byte parentFace = NO_PARENT;
-byte sendingFace = 0;
-bool isSending = false;
+static byte parentFace = NO_PARENT;
+static byte sendingFace = 0;
+static bool isSending = false;
 
 ////Map////
-byte north = 0;
-bool isNorthYellow = false;
-byte location[2];
-const byte MAP_HEIGHT = 16;
-const byte MAP_WIDTH = 16;
-byte mineMap[MAP_HEIGHT][MAP_WIDTH] = {
-    {EMPTY, MINE, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, EMPTY},
-    {EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, END, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, MINE, MINE, EMPTY, MINE, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, START, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY}
-  };
-byte currentRow = 0;
+static byte north = 0;
+static byte mineCount = 0;
+static byte mineIndicatorFace = 0;
+bool isEdge = false;
+static Timer spinTimer;
+static char location[2];
+static byte mineMap[MINE_MAX][2];
+/*static byte mineMap[MAP_HEIGHT][MAP_WIDTH] = {
+    {EMPTY, MINE, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY},
+    {EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, END, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY, MINE},
+    {EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, MINE, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, MINE, MINE, EMPTY, MINE, MINE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, MINE, EMPTY, EMPTY, EMPTY, EMPTY, MINE},
+    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, START, EMPTY, EMPTY, MINE}
+  };*/
+static byte currentMine = 0;
 
 void setup() {
   randomize(); //Seed random generator
@@ -81,6 +88,19 @@ void loop() {
       break;
   }
 
+  if (buttonMultiClicked()) {
+    gameEnd(false);
+  }
+
+  if (mineCount > 0 && spinTimer.isExpired()) {
+    spinTimer.set(SPIN_TIMER_LENGTH);
+    setColor(EMPTY_COLOR);
+    for (byte i = 0; i < mineCount; i++) {
+      setColorOnFace(MINE_INDICATOR_COLOR, (mineIndicatorFace + i) % FACE_COUNT);
+    }
+    mineIndicatorFace = (mineIndicatorFace + 1) % 6;
+  }
+  
   buttonDoubleClicked(); //Resets double click flag, just in case
 }
 
@@ -92,13 +112,78 @@ void createMap() {
   //Randomly decide where the start and end are
   //Randomly place out mines in other spaces
   //Need to determine where current blink is, and make sure no others are where a bomb would be...  
+  mineMap[0][0] = 1;
+  mineMap[0][1] = 15;
+  mineMap[1][0] = 3;
+  mineMap[1][1] = 15;
+  mineMap[2][0] = 5;
+  mineMap[2][1] = 15;
+  mineMap[3][0] = 10;
+  mineMap[3][1] = 15;
+  mineMap[4][0] = 2;
+  mineMap[4][1] = 14;
+  mineMap[5][0] = 5;
+  mineMap[5][1] = 14;
+
+  mineMap[6][0] = 6;
+  mineMap[6][1] = 8;
+  mineMap[7][0] = 9;
+  mineMap[7][1] = 8;
+  mineMap[8][0] = 11;
+  mineMap[8][1] = 8;
+
+  mineMap[9][0] = 2;
+  mineMap[9][1] = 7;
+  mineMap[10][0] = 6;
+  mineMap[10][1] = 7;
+  mineMap[11][0] = 8;
+  mineMap[11][1] = 7;
+
+  mineMap[12][0] = 7;
+  mineMap[12][1] = 6;
+  mineMap[13][0] = 10;
+  mineMap[13][1] = 6;
+
+  mineMap[14][0] = 2;
+  mineMap[14][1] = 4;
+  mineMap[15][0] = 3;
+  mineMap[15][1] = 4;
+  mineMap[16][0] = 5;
+  mineMap[16][1] = 4;
+  mineMap[17][0] = 6;
+  mineMap[17][1] = 4;
+
+  mineMap[18][0] = 6;
+  mineMap[18][1] = 3;
+  mineMap[19][0] = 8;
+  mineMap[19][1] = 3;
+
+
+  mineMap[20][0] = 5;
+  mineMap[20][1] = 2;
+  mineMap[21][0] = 5;
+  mineMap[21][1] = 2;
+
+  mineMap[22][0] = 6;
+  mineMap[22][1] = 1;
+  mineMap[23][0] = 11;
+  mineMap[23][1] = 1;
+
+  mineMap[24][0] = 11;
+  mineMap[24][1] = 0;
+
+  mineMap[25][0] = 13;
+  mineMap[25][1] = 15;
+  mineMap[26][0] = 14;
+  mineMap[26][1] = 9;
+  mineMap[27][0] = 14;
+  mineMap[27][1] = 5;
 }
 
 void sendingLoop() {
   if (!isSending) {
     for (byte f = sendingFace; f < FACE_COUNT; f++) {
-      if (!isValueReceivedOnFaceExpired(f) && (getLastValueReceivedOnFace(f) == SETUP || 
-        getLastValueReceivedOnFace(f) == GAME_OVER || getLastValueReceivedOnFace(f) == VICTORY)) {
+      if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) == SETUP) {
         sendingFace = f;
         isSending = true;
         setValueSentOnFace(SENDING, f);
@@ -108,16 +193,17 @@ void sendingLoop() {
   }
 
   if (isSending) {
-    if (currentRow < MAP_HEIGHT) {
-      if (!isValueReceivedOnFaceExpired(sendingFace) && getLastValueReceivedOnFace(sendingFace) == currentRow) {
-        sendDatagramOnFace(&mineMap[currentRow++], MAP_WIDTH, sendingFace);
+    if (currentMine < MINE_MAX) {
+      if (!isValueReceivedOnFaceExpired(sendingFace) && getLastValueReceivedOnFace(sendingFace) == currentMine) {
+        sendDatagramOnFace(&mineMap[currentMine], 2, sendingFace);
+        ++currentMine;
       }
-      setColor(dim(GREEN, map(currentRow, 0, MAP_HEIGHT, 0, 255)));
+      setColor(dim(GREEN, map(currentMine, 0, MINE_MAX, 0, 255)));
       flashColorOnFace(BLUE, sendingFace);
     }
     else {
       if (!isValueReceivedOnFaceExpired(sendingFace) && getLastValueReceivedOnFace(sendingFace) == READY) {
-        currentRow = 0;
+        currentMine = 0;
         isSending = false;
       }
     }
@@ -134,48 +220,43 @@ void sendingLoop() {
 }
 
 void receivingLoop() {
-  setValueSentOnFace(currentRow, parentFace);
+  setValueSentOnFace(currentMine, parentFace);
   if (isDatagramReadyOnFace(parentFace)) {
     byte *datagram = getDatagramOnFace(parentFace);
     byte dataLength = getDatagramLengthOnFace(parentFace);
-    if (dataLength == MAP_WIDTH) {
+    if (dataLength == 2) {
       markDatagramReadOnFace(parentFace);
       for (byte i=0; i < dataLength; i++) {
-        mineMap[currentRow][i] = *(datagram + i);
+        mineMap[currentMine][i] = *(datagram + i);
       }
-      ++currentRow;
-      setValueSentOnFace(currentRow, parentFace);
+      ++currentMine;
+      setValueSentOnFace(currentMine, parentFace);
     }
     else {
       setColor(ORANGE);
       return;
     }
   }
-  if (currentRow >= MAP_HEIGHT) {
+  if (currentMine >= MINE_MAX) {
     state = SENDING;
-    currentRow = 0;
+    currentMine = 0;
   }
-  setColor(dim(RED, map(currentRow, 0, MAP_HEIGHT, 0, 255)));
+  setColor(dim(RED, map(currentMine, 0, MINE_MAX, 0, 255)));
   flashColorOnFace(BLUE, parentFace);
 }
 
 void readyLoop() {
+  location[0] = 0;
+  location[1] = 0;
   //If master, tell all to begin game by sending location data
   if (parentFace == MASTER) {
-    for (byte y = 0; y < MAP_HEIGHT; y++) {
-        for (byte x = 0; x < MAP_WIDTH; x++) {
-          if (mineMap[y][x] == START) {
-            location[0] = x;
-            location[1] = y;
-            setValueSentOnAllFaces(PLAYING);
-            sendLocationData();
-            state = CONNECTED;
-            setColor(OFF);
-            setColorOnFace(BLUE, north);
-            return;
-          }
-        }
-      }
+    location[0] = 8;
+    location[1] = 0;
+    setValueSentOnAllFaces(PLAYING);
+    state = CONNECTED;
+    setColor(EMPTY_COLOR);
+    setColorOnFace(BLUE, north);
+    return;
   }
   //Otherwise listen for start signal
   if (getLocation()) {
@@ -185,13 +266,13 @@ void readyLoop() {
     sendLocationData();
   }
 
-  flashColorOnFace(GREEN, 6);
+  flashColorOnFace(READY_GREEN, 6);
 }
 
 void standbyLoop() {
   //Wait for starting press
   if (buttonDoubleClicked()) {
-    //createMap();
+    createMap();
     state = SENDING;
     parentFace = MASTER;
     setValueSentOnAllFaces(SENDING_TO_OTHER);
@@ -202,13 +283,14 @@ void standbyLoop() {
     if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) == SENDING) {
       parentFace = f;
       state = RECEIVING;
-      setValueSentOnAllFaces(currentRow);
+      setValueSentOnAllFaces(currentMine);
       return;
     }
   }
 
   //Set lights
   flashColorOnFace(BLUE, 6);
+  setColorOnFace(GREEN, north); //For debugging
 }
 
 
@@ -217,54 +299,55 @@ void standbyLoop() {
 ////////////////////
 
 void disconnectedLoop() {
-  if (buttonLongPressed()) {
-    gameEnd(false);
-  }
-  
   //Check if we have a location signal
+  mineCount = 0;
+  isEdge = false;
   flashColorOnFace(CYAN, 6);
+  
   if (getLocation()) {
     //Found a face transmitting a location!
     state = CONNECTED;
-    if (!((0 <= location[0] && location[0] < MAP_WIDTH) && (0 <= location[1] && location[1] < MAP_HEIGHT))) {
+    byte space = EMPTY;
+    if (location[1] > MAP_HEIGHT) {
+      space = END;
+    }
+    else if (location[0] < 0 || location[0] >= MAP_WIDTH || location[1] < 0) {
       setColor(OFF);
-      setColorOnFace(northColor, north);
+      setColorOnFace(NORTH_COLOR, north);
+      isEdge = true;
       return;
     }
-    else {setColor(emptyColor);}
-    Color flashColor = YELLOW;
-    byte space = mineMap[location[1]][location[0]];
+    else {
+      for (byte i = 0; i < MINE_MAX; ++i) {
+        if (mineMap[i][0] == location[0] && mineMap[i][1] == location[1]) {
+          space = MINE;
+        }
+      }
+    }
     if (space == EMPTY) {
-      byte mineCount = 0;
-      if (location[1] + 1 < MAP_HEIGHT && mineMap[location[1] + 1][location[0]] == MINE) {
-        ++mineCount;
-      }
-      if (location[1] - 1 >= 0 && mineMap[location[1] - 1][location[0]] == MINE) {
-        ++mineCount;
-      }
-      if (location[0] - 1 >= 0 && mineMap[location[1]][location[0] - 1] == MINE) {
-        ++mineCount;
-      }
-      if (location[0] + 1 < MAP_HEIGHT && mineMap[location[1]][location[0] + 1] == MINE) {
-        ++mineCount;
-      }
-      int rowModifier = -1;
+      setColor(EMPTY_COLOR);
+      char rowModifier = 1;
       if ((location[0] % 2) == 0) { //Even column
-        rowModifier = 1;
+        rowModifier = -1;
       }
-      if (rowModifier == -1) {
-      }
-      if ((location[0] - 1 >= 0) && (0 <= (location[1] + rowModifier)) && ((location[1] + rowModifier) < MAP_HEIGHT) && (mineMap[location[1] + rowModifier][location[0] - 1] == MINE)) {
-        ++mineCount;
-      }
-      if ((location[0] + 1 < MAP_WIDTH) && (0 <= (location[1] + rowModifier)) && ((location[1] + rowModifier) < MAP_HEIGHT) && (mineMap[location[1] + rowModifier][location[0] + 1] == MINE)) {
-        ++mineCount;
-      }
-      isNorthYellow = false;
-      for (byte i = 0; i < mineCount; i++) {
-        setColorOnFace(flashColor, i);
-        if (i == north) {
-          isNorthYellow = true;
+      for (byte i = 0; i < MINE_MAX; ++i) {
+        if (location[0] == mineMap[i][0] && location[1] - 1 == mineMap[i][1]) {//location[1] - 1 >= 0 && mineMap[location[1] - 1][location[0]] == MINE) {
+          ++mineCount;
+        }
+        else if (location[0] == mineMap[i][0] && location[1] + 1 == mineMap[i][1]) {//location[1] + 1 < MAP_HEIGHT && mineMap[location[1] + 1][location[0]] == MINE) {
+          ++mineCount;
+        }
+        else if (location[0] == mineMap[i][0] - 1 && location[1] == mineMap[i][1]) {//location[0] - 1 >= 0 && mineMap[location[1]][location[0] - 1] == MINE) {
+          ++mineCount;
+        }
+        else if (location[0] == mineMap[i][0] + 1 && location[1] == mineMap[i][1]) {//location[0] + 1 < MAP_HEIGHT && mineMap[location[1]][location[0] + 1] == MINE) {
+          ++mineCount;
+        }
+        else if (location[0] == mineMap[i][0] - 1 && location[1] + rowModifier == mineMap[i][1]) {//(location[0] - 1 >= 0) && (0 <= (location[1] + rowModifier)) && ((location[1] + rowModifier) < MAP_HEIGHT) && (mineMap[location[1] + rowModifier][location[0] - 1] == MINE)) {
+          ++mineCount;
+        }
+        else if (location[0] == mineMap[i][0] + 1 && location[1] + rowModifier == mineMap[i][1]) {//(location[0] + 1 < MAP_WIDTH) && (0 <= (location[1] + rowModifier)) && ((location[1] + rowModifier) < MAP_HEIGHT) && (mineMap[location[1] + rowModifier][location[0] + 1] == MINE)) {
+          ++mineCount;
         }
       }
     }
@@ -278,21 +361,16 @@ void disconnectedLoop() {
 }
 
 void connectedLoop() {
-  //TODO: Make an indication that reset is about to happen
-  if (buttonLongPressed()) {
-    gameEnd(false);
-  }
-
   if (buttonPressed()) {
-    setColorOnFace(northColor, north);
+    setColorOnFace(NORTH_COLOR, north);
   }
-  if (buttonReleased()) {
-    Color revertColor = emptyColor;
+  /*if (buttonReleased()) {
+    Color revertColor = EMPTY_COLOR;
     if (isNorthYellow) {
       revertColor = YELLOW;
     }
     setColorOnFace(revertColor, north);
-  }
+  }*/
   
   //Listen for end signal
   FOREACH_FACE(f) {
@@ -312,18 +390,24 @@ void connectedLoop() {
     }
     state = DISCONNECTED;
   }
-  sendLocationData();
+  if (!isEdge) {
+    sendLocationData();
+  }
 }
 
 void gameEndLoop(bool won) {
   //Reset data
-  currentRow = 0;
+  location[0] = location[1] = 0;
+  currentMine = 0;
   sendingFace = 0;
+  mineCount = 0;
+  mineIndicatorFace = 0;
   isSending = false;
   FOREACH_FACE(f) {
     markDatagramReadOnFace(f);
   }
 
+  
   //Check if timer has expired
   if (endTimer.isExpired()) {
     setColor(BLUE);
@@ -347,46 +431,51 @@ void sendLocationData() {
   //They "bend" up when moving from an even to an odd row, (counting 0 as even)
   //and down when moving from an odd to an even row.
   bool evenColumn = location[0]%2 == 0;
+  byte data[3];
+  byte newX;
+  byte newY;
   FOREACH_FACE(f) {
-    byte newX = location[0];
-    byte newY = location[1];
-      //Sends location, and north data
-      byte faceCorrectedForNorth = (f + FACE_COUNT - north) % 6;
-      switch (faceCorrectedForNorth) {
-        case 0:
-          --newY;
-          break;
-        case 1:
-          ++newX;
-          if (!evenColumn) {
-            --newY;
-          }
-          break;
-        case 2:
-          ++newX;
-          if (evenColumn) {
-            ++newY;
-          }
-          break;
-        case 3:
+    newX = location[0];
+    newY = location[1];
+    //Sends location, and north data
+    byte faceCorrectedForNorth = (f + FACE_COUNT - north) % 6;
+    switch (faceCorrectedForNorth) {
+      case 0:
+        ++newY;
+        break;
+      case 1:
+        ++newX;
+        if (!evenColumn) {
           ++newY;
-          break;
-        case 4:
-          --newX;
-          if (evenColumn) {
-            ++newY;
-          }
-          break;
-        case 5:
-          --newX;
-          if (!evenColumn) {
-            --newY;
-          }
-          break;
-      }
-      byte data[] = {faceCorrectedForNorth , newX, newY};
-      sendDatagramOnFace(&data, sizeof(data), f);
+        }
+        break;
+      case 2:
+        ++newX;
+        if (evenColumn) {
+          --newY;
+        }
+        break;
+      case 3:
+        --newY;
+        break;
+      case 4:
+        --newX;
+        if (evenColumn) {
+          --newY;
+        }
+        break;
+      case 5:
+        --newX;
+        if (!evenColumn) {
+          ++newY;
+        }
+        break;
     }
+    data[0] = faceCorrectedForNorth;
+    data[1] = newX;
+    data[2] = newY;
+    sendDatagramOnFace(&data, sizeof(data), f);
+  }
 }
 
 //Returns boolean if location data was properly retrieved or not. 
@@ -421,5 +510,5 @@ void gameEnd(bool won) {
     state = GAME_OVER;
   }
   setValueSentOnAllFaces(state);
-  endTimer.set(TIMER_LENGTH);
+  endTimer.set(END_TIMER_LENGTH);
 }
